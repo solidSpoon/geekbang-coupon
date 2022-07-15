6600411826780e20b4cba49d0e87cbcf304c8889
cedarrival <cedarrival@foxmail.com>
2022-07-15 11:12:47 +0800

    feat: init 实现金丝雀测试

--- coupon-customer-serv/coupon-customer-impl/src/main/java/xyz/solidspoon/coupon/customer/Application.java
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/xyz/solidspoon/coupon/customer/Application.java
@@ -3,10 +3,12 @@ package xyz.solidspoon.coupon.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import xyz.solidspoon.coupon.customer.loadbalance.CanaryRuleConfiguration;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -16,6 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = {"xyz.solidspoon"})
 //用于扫描JPA实体类 @Entity，默认扫本包当下路径
 @EntityScan(basePackages = {"xyz.solidspoon"})
+@LoadBalancerClient(value = "coupon-template-serv", configuration = CanaryRuleConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
